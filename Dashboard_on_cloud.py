d1ed7de7604c2ba309b746b383b229501ab20bf1
Ahmed Yasser <72948428+ahmedyasser-ai@users.noreply.github.com>
2024-05-10 06:40:39 +0300

    Adding requirements, python and csv files

---
+++ Dashboard_on_cloud.py
@@ -0,0 +1,50 @@
+
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import streamlit as st
+
+#reading our dataset
+df = pd.read_csv("new_sales_store_data.csv")
+
+st.title("Sales Store Analysis")
+
+st.header("Metrics")
+
+c1 ,c2 , c3=st.columns(3)
+with c1:
+    st.metric(label = "Total Sales in M$" , value = (df["Sales"].sum() / 1000000).round(3) )
+with c2:
+    st.metric(label = "Total Profit in M$" , value = (df["Profit"].sum() / 1000000).round(3) )
+with c3:
+    st.metric(label = "Avg Discount Percentage Per Year %" , value = 15.5 )
+
+
+
+st.header("Best Sellers Chart")
+
+sub_cat_index = df["Sub-Category"].value_counts().head().index
+
+sub_cat_df = df[ df["Sub-Category"].isin(sub_cat_index) ]
+
+px1 = px.histogram(sub_cat_df , x = "Sub-Category" ,text_auto = True).update_xaxes(categoryorder = 'total descending')
+
+st.plotly_chart(px1)
+
+
+
+st.header("Category's Profit Over Regions")
+
+pivot_table = pd.pivot_table(df , index = "Category" , columns = "Region" , values = "Profit" , aggfunc = sum )
+
+c1,c2 = st.columns([4,1])
+
+#heat map or bar plot to draw pivot table
+with c1:
+    px2 = px.imshow(pivot_table , color_continuous_scale=px.colors.diverging.RdBu)
+    st.plotly_chart(px2)
+    
+    
+with c2:
+    px3 = px.bar(data_frame = df , x = "Category" , y = "Profit" ,color = "Region",barmode = "group")
+    st.plotly_chart(px3)
